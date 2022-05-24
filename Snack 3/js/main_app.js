064d971d52bd6f383db72db1f36c93b29239bee6
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-24 17:55:51 +0200

    aggiunta prima parte snack 3

--- Snack 3/js/main_app.js
+++ Snack 3/js/main_app.js
@@ -2,3 +2,11 @@
 Snack3
 -Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari
 */
+
+//Creo il mio array di numeri interi
+const numeriInteri= [1,2,10,24,56,77,99,23,45]
+
+// creo il mio ciclo
+for(let i=0; i< numeriInteri.length;i++){
+    
+}
