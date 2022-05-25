d87045bffc337194ca561b765cf1e1e7d389c0eb
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-25 10:23:20 +0200

    aggiunto snack 3 completo

--- Snack 3/js/main_app.js
+++ Snack 3/js/main_app.js
@@ -10,4 +10,24 @@ console.log(numeriInteri)
 // creo il mio ciclo
 for(let i=0; i< numeriInteri.length;i++){
     
+
+// Creo degli indici e degli array vuoti
+let indicePari = false
+let somma = 0
+
+const pari = []
+const dispari = []
+
+//creo il mio ciclo per controllare se i miei numeri sono in indice pari o indice dispari
+for (let i = 0; i < numeriInteri.length; i++) {
+    if (i % 2 === 0) {
+        indicePari = true
+        console.log(numeriInteri[i], indicePari)
+        pari.push(numeriInteri[i])
+    } else {
+        dispari.push(numeriInteri[i])
+        somma += numeriInteri[i]
+    }
+}
+console.log(pari,dispari,somma)
 }
